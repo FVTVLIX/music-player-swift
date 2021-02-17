bdf5e1c50c4d5aae3fb376668255aad05b9cca77
FVTVLIX <andman290@gmail.com>
2021-02-17 16:48:54 -0600

    Initial project setup

---
+++ Shared/Music_PlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Music_PlayerApp.swift
+//  Shared
+//
+//  Created by Dre on 1/31/21.
+//
+
+import SwiftUI
+
+@main
+struct Music_PlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
